2bc538cb29b7b4cce3b70085d99e9b588918c8f2
Samuel2143 <samuel2143ip@gmail.com>
2023-10-23 00:41:49 +0530

    Two methods to solve this problem

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,3 +1,6 @@
+//There are two methods for solving this problem..
+// Method 1:
+
 import java.util.*;
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
@@ -14,6 +17,35 @@ public class LongestCommonPrefix {
             }
         }
         return res.toString();
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] strs = new String[n];
+        for(int i=0;i<n;i++){
+            strs[i]=sc.nextLine();
+        }
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
+
+// Method 2:
+
+import java.util.*;
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        int index = 0;
+        while(index<first.length()&&index<last.length()){
+            if(first.charAt(index)==last.charAt(index)){
+                index++;
+            }
+            else break;
+        }
+        return first.substring(0,index);
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
