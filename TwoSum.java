c605a39bdc9a6fdf0cc16c73041c27f131720090
Samuel2143 <samuel2143ip@gmail.com>
2023-10-22 19:58:07 +0530

    Two sum java program

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+}
